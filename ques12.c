3ba9e3ec0245534667b7e8b61c18c74fb069b68c
Aadithyan <rak34316@gmail.com>
2024-10-18 19:40:38 +0530

    natural numbers up to N without using a semi-colon.

---
+++ ques12.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#define N 10
+
+int main(int val)
+{
+ if (val <= N && printf("%d ", val) && main(val + 1)) {
+ }
+}// C program to print
+// all natural numbers
+// upto N without using semi-colon
+#include <stdio.h>
+#define N 10
+
+int main(int val)
+{
+ if (val <= N && printf("%d ", val) && main(val + 1)) {
+ }
+}
